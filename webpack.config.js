1b46801cf24ae78ec8a5f5b4a121003f296eb62f
wangyunhui <1825548604@qq.com>
2019-07-19 21:24:00 +0800

    可能有错误

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');    //node.js里面一个基本的包
+const HTMLPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const wbpack = require("webpack")
+// 属性NODE_ENV实在package.json文件里定义的  定义的属性可以在process.env对象里取
+// 判断当前的环境是否是开发环境
+const isDev = process.env.NODE_ENV === 'development'
 // __dirname:当前文件所在的目录，也就是根目录
-module.exports = {
+const config = {
+    target:'web',
     entry: path.join(__dirname, 'src/index.js'),  //入口文件
     output: {
         filename: 'bundle.js',   //出口文件
@@ -43,6 +49,24 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new wbpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
     ] 
 }
+
+if(isDev) {
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',  //设置为0.0.0.0的好处是不仅能用losthost访问，还能用本机的IP：127.0.0.1来访问，也可以在局域网内输入ip地址来访问。（终于知道了之前为什么同事不能访问我的项目了）
+        overlay: {
+            errrors: true,
+        }
+    }
+}
+
+module.exports = config
